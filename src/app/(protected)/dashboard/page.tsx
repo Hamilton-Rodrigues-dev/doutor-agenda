1a63228cd14839eb71ae223ebf1ac365cb81cfaa
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-06-14 23:16:30 -0400

    feat: add data table to appointments today

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import dayjs from "dayjs";
 import { and, count, desc, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { CalendarIcon } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageActions,
   PageContainer,
@@ -16,6 +19,7 @@ import { db } from "@/db";
 import { appointmentsTable, doctorsTable, patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
+import { appointmentsTableColumns } from "../appointments/_components/table-columns";
 import AppointmentsChart from "./components/appointments-chart";
 import { DatePicker } from "./components/date-picker";
 import StatsCards from "./components/stats-cards";
@@ -59,6 +63,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
     [totalDoctors],
     topDoctors,
     topSpecialties,
+    todayAppointments,
   ] = await Promise.all([
     db
       .select({
@@ -133,6 +138,17 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
       )
       .groupBy(doctorsTable.specialty)
       .orderBy(desc(count(appointmentsTable.id))),
+    db.query.appointmentsTable.findMany({
+      where: and(
+        eq(appointmentsTable.clinicId, session.user.clinic.id),
+        gte(appointmentsTable.date, new Date()),
+        lte(appointmentsTable.date, new Date()),
+      ),
+      with: {
+        doctor: true,
+        patient: true,
+      },
+    }),
   ]);
 
   const chartStartDate = dayjs().subtract(10, "days").startOf("day").toDate();
@@ -181,10 +197,20 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
           <TopDoctors doctors={topDoctors} />
         </div>
         <div className="grid grid-cols-[2.25fr_1fr] gap-4">
-          {/* <DataTable
+          <Card>
+            <CardHeader>
+              <div className="flex items-center gap-3">
+                <CalendarIcon className="text-muted-foreground" />
+                <CardTitle className="text-base">Agendamento de hoje</CardTitle>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <DataTable
                 columns={appointmentsTableColumns}
                 data={todayAppointments}
-              /> */}
+              />
+            </CardContent>
+          </Card>
           <TopSpecialties topSpecialties={topSpecialties} />
         </div>
       </PageContent>
