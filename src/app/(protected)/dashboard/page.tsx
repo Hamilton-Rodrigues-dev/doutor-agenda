fa7ff2a153a7ea5f30ea198006dd4bca99a9e698
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-06-14 21:30:54 -0400

    feat: add stats cards to dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+import { and, count, eq, gte, lte, sum } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -10,11 +12,21 @@ import {
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
+import { db } from "@/db";
+import { appointmentsTable, doctorsTable, patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import { DatePicker } from "./components/date-picker";
+import StatsCards from "./components/stats-cards";
 
-const DashboardPage = async () => {
+interface DashboardPageProps {
+  searchParams: Promise<{
+    from: string;
+    to: string;
+  }>;
+}
+
+const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -26,6 +38,57 @@ const DashboardPage = async () => {
     redirect("/clinic-form");
   }
 
+  if (!session?.user.clinic.id) {
+    redirect("/clinic-form");
+  }
+  const { from, to } = await searchParams;
+
+  if (!from || !to) {
+    redirect(
+      `dashboard?from=${dayjs().format("YYYY-MM-DD")}&to=${dayjs().add(1, "month").format("YYYY-MM-DD")}`,
+    );
+  }
+
+  const [[totalRevenue], [totalAppointments], [totalPatients], [totalDoctors]] =
+    await Promise.all([
+      db
+        .select({
+          total: sum(appointmentsTable.appointmentPriceInCents),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, session.user.clinic.id),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(appointmentsTable.id),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, session.user.clinic.id),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(),
+        })
+        .from(patientsTable)
+        .where(eq(patientsTable.clinicId, session.user.clinic.id)),
+      db
+        .select({
+          total: count(),
+        })
+        .from(doctorsTable)
+        .where(eq(doctorsTable.clinicId, session.user.clinic.id)),
+    ]);
+
   return (
     <PageContainer>
       <PageHeader>
@@ -36,10 +99,14 @@ const DashboardPage = async () => {
         <PageActions>
           <DatePicker />
         </PageActions>
-      </PageHeader>
-
+      </PageHeader>{" "}
       <PageContent>
-        <></>
+        <StatsCards
+          totalRevenue={totalRevenue.total ? Number(totalRevenue.total) : null}
+          totalAppointments={totalAppointments.total}
+          totalPatients={totalPatients.total}
+          totalDoctors={totalDoctors.total}
+        />
       </PageContent>
     </PageContainer>
   );
