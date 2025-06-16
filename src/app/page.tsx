d8d248ded9752225ecccdcb9290d34331da2733f
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-06-16 17:49:36 -0400

    "refactor: redirect home"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { auth } from "@/lib/auth";
-
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session?.user) {
-    redirect("/authentication");
-  }
-
-  if (!session?.user.clinic) {
-    redirect("/clinic-form");
-  }
-
-  if (!session.user.plan) {
-    redirect("/new-subscription");
-  }
+  redirect("/dashboard");
 }
